0608024bc8ca6396d917daeac7bb4d66fa876998
soilapertet <nicolepertet@gmail.com>
2022-03-09 17:20:13 +0300

    Build and append the weather cards to the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 const form = document.querySelector(".weather-form");
 const input = document.querySelector("input");
+let weatherIcon;
 
 const errorMessage = document.querySelector(".error-msg");
-
-// const apiKey = "336a341afbdb8822005a1515ab344ce6";
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputtedCity}&appid=${apiKey}&units=metric`;
 
 form.addEventListener("submit", (event) => {
 
@@ -12,7 +10,6 @@ form.addEventListener("submit", (event) => {
 
   const inputtedCity = input.value;
   const apiKey = "336a341afbdb8822005a1515ab344ce6";
-  // const apiKey = "4d8fb5b93d4af21d66a2948710284366";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputtedCity}&appid=${apiKey}&units=metric`;
   
   if(inputtedCity === "") {
@@ -23,11 +20,36 @@ form.addEventListener("submit", (event) => {
   fetch(url)
   .then(response => response.json()) // convert the response data to the desired JSON format
   .then(data => {
-    // data will be available for manipulation
-    // do stuff with data
+
+    const { main, name, sys, weather } = data;
+    
+    const section = document.createElement("section");
+    section.classList.add("city");
+    const weatherContent = 
+    `
+    <h2 class="city-name" data-name="${name}, ${sys.country}">
+      <span>${name}</span>
+      <sup>${sys.country}</sup>
+    </h2>
+    <div class="city-temp">
+      ${Math.round(main.temp)}<sup>°C</sup>
+      <span class="high-low-temp">H: ${Math.round(main.temp_max)}<sup>°C</sup> | L: ${Math.round(main.temp_min)}<sup>°C</sup></span>
+    </div>
+    <figure>
+      <img class="city-icon" src=${weatherIcon} alt="${weather[0]["main"]}">
+      <figcaption>${weather[0]["description"]}</figcaption>
+    </figure>
+    `;
+    section.innerHTML = weatherContent;
+
+    const cities = document.querySelector("#cities");
+    cities.append(section);
+
+    form.reset();
+    input.focus();
   })
   .catch(() => {
-    errorMessage.textContent = "Please search for a valid city 😩";
+    errorMessage.textContent = "* Please search for a valid city.";
   });
 });
 
